1c8e0f4b25631718f8c5bae6e46052ce16f9fad7
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2023-12-01 17:30:05 +1030

    day 1 solutions

---
+++ day1.js
@@ -0,0 +1,33 @@
+// const input = ``; // sample input
+const input = document.body.innerText.trim();
+const data = input.split("\n");
+
+const getCalibrationTotal = (values) => {
+    return values.reduce((sum, current) =>  sum + current.at(0) * 10 + current.at(-1)
+    , 0)
+}
+
+const part1 = (data) => {
+  const numbers = data.map(line => line.match(/\d/g)?.map(Number) || [0]);
+  console.log(getCalibrationTotal(numbers))
+};
+
+const part2 = (data) => {
+    const letterNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    // using matchAll with capture group since match only gives ['one'] for eg 'oneeight'
+    // but we need ['one', 'eight'] for eg 'oneeight'
+    // https://stackoverflow.com/a/33903830
+
+    const numbers = data
+        .map(line => [...line.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g)].map(match => match[1])
+            .map(n => /\d/.test(n) ? Number(n) : letterNumbers.indexOf(n) + 1))
+    console.log(getCalibrationTotal(numbers))
+};
+
+console.time("part1");
+part1(data);
+console.timeEnd("part1");
+console.time("part2");
+part2(data);
+console.timeEnd("part2");
