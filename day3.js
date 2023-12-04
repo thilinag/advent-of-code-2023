05e40892d317da5efb0529015f446ca8bcbf1281
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2023-12-04 11:43:03 +1030

    day 3 part 2

--- day3.js
+++ day3.js
@@ -99,8 +99,90 @@ const part1 = () => {
     return partNumbers.reduce((sum, current) => sum + Number(current), 0);
 };
 
+const getGearLocations = (engine, row, column, currentNumberLength) => {
+    const possibleGearLocations = [];
+    for (let i = column; i > column - currentNumberLength; i--) {
+        const gearLocations = getAdjacent(engine, row, i, true).filter(
+            (value) => {
+                const [symbol, starRow, starColumn] = value.split('-');
+                return (
+                    symbol === '*' &&
+                    getAdjacent(
+                        engine,
+                        Number(starRow),
+                        Number(starColumn),
+                    ).filter((value) => !isNaN(value)).length > 1
+                );
+            },
+        );
+        possibleGearLocations.push(...gearLocations);
+    }
+
+    return possibleGearLocations;
+};
+
 const part2 = () => {
     const data = getData(2);
+
+    const engine = data.map((row) => row.split(''));
+    const partNumbers = new Map();
+
+    for (let row = 0; row < engine.length; row++) {
+        let currentNumber = '';
+        let isPartNumber = [];
+        const columnCount = engine[row].length;
+        for (let column = 0; column < columnCount; column++) {
+            const currentChar = engine[row][column];
+
+            if (!isNaN(currentChar)) {
+                currentNumber += currentChar;
+                isPartNumber.push(hasAdjacentSymbols(engine, row, column));
+            }
+
+            if (isNaN(currentChar) || column + 1 === columnCount) {
+                if (isPartNumber.some((i) => i)) {
+                    const gearLocations = getGearLocations(
+                        engine,
+                        row,
+                        column - 1,
+                        currentNumber.length,
+                    );
+
+                    gearLocations.forEach((location) => {
+                        // save gear location with row so we can check later
+                        if (location.length) {
+                            partNumbers.set(
+                                location,
+                                partNumbers.has(location)
+                                    ? [
+                                          ...partNumbers.get(location),
+                                          `${currentNumber}-${row}`,
+                                      ]
+                                    : [`${currentNumber}-${row}`],
+                            );
+                        }
+                    });
+                }
+
+                currentNumber = '';
+                isPartNumber = [];
+            }
+        }
+    }
+
+    const gearRatios = [];
+
+    partNumbers.forEach((values, key) => {
+        const uniqueValues = [...new Set(values)];
+        if (uniqueValues.length === 2) {
+            gearRatios.push(
+                Number(uniqueValues[0].split('-')[0]) *
+                    Number(uniqueValues[1].split('-')[0]),
+            );
+        }
+    });
+
+    return gearRatios.reduce((sum, current) => sum + Number(current), 0);
 };
 
 console.time('part1');
